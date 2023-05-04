edba4533636afaeecaf5182028632be260dc6443
bobur <usmonkulov5@gmail.com>
2023-05-04 11:01:24 +0500

    id_reply

--- main.py
+++ main.py
@@ -13,7 +13,16 @@ def id(message):
     typ = message.chat.type
     dat = message.date
 
-    bot.send_message(message.chat.id, text="ID : {}\nName : {}\nUser : @{}\nType : {}\nDate Msg : {}\nChannel : [Bobur Usmonkulov | Dev](t.me/bobur_usmonkulov_blog)".format(cht, frt, usr, typ, dat))
+    bot.send_message(message.chat.id,
+                     text="ID : {}\nName : {}\nUser : @{}\nType : {}\nDate Msg : {}\nChannel : [Bobur Usmonkulov | Dev](t.me/bobur_usmonkulov_blog)".format(
+                         cht, frt, usr, typ, dat), disable_web_page_preview="true")
+
+
+@bot.message_handler(regexp="^id")
+def reply(message):
+    if message.reply_to_message:
+        id = message.reply_to_message.from_user.id
+        bot.send_message(message.chat.id, id)
 
 
 bot.polling(True)
